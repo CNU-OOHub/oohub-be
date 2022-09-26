c909ca7093cda40a020c4527665e6ea3f384c505
Songyi Kim <ksl2950@naver.com>
2022-09-26 20:06:48 +0900

    init: project setting

---
+++ src/main/java/com/sudoku/oohub/OohubApplication.java
@@ -0,0 +1,13 @@
+package com.sudoku.oohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudoku/oohub/OohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoku.oohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
