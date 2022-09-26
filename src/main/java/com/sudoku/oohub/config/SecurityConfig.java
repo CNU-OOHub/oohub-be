6077e6eeb88f9b517706a68f001d704e5ae74d5e
Songyi Kim <ksl2950@naver.com>
2022-09-26 21:50:45 +0900

    chore: security config 추가

---
+++ src/main/java/com/sudoku/oohub/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.sudoku.oohub.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .addFilter(corsFilter)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용하지 않겠다.
+                .and().formLogin().disable()
+                .httpBasic().disable() // 자체 사용자 인증 x
+
+                .addFilter(corsFilter) // 모든 요청 허용 (CrossOrigin 인증 x)
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/v1/login").permitAll()
+                .antMatchers("/api/v1/join").permitAll()
+
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
