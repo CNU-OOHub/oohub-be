a78cbc97ec82004fb6b50befaa6d102c18f329f4
GwakMirae <alfo9490@naver.com>
2022-10-06 00:05:35 +0900

    feat: modify member service (#18)

--- src/main/java/com/sudoku/oohub/domain/Member.java
+++ src/main/java/com/sudoku/oohub/domain/Member.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
--- src/main/java/com/sudoku/oohub/service/MemberService.java
+++ src/main/java/com/sudoku/oohub/service/MemberService.java
@@ -1,17 +1,22 @@
 package com.sudoku.oohub.service;
 
+import com.sudoku.oohub.converter.Converter;
 import com.sudoku.oohub.domain.Department;
 import com.sudoku.oohub.domain.Member;
 import com.sudoku.oohub.domain.Role;
 import com.sudoku.oohub.dto.request.CreateMemberDto;
+import com.sudoku.oohub.dto.response.MemberDto;
 import com.sudoku.oohub.exception.DuplicateMemberException;
 import com.sudoku.oohub.repository.DepartmentRepository;
 import com.sudoku.oohub.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +26,7 @@ public class MemberService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final MemberRepository memberRepository;
     private final DepartmentRepository departmentRepository;
+    private final Converter converter;
 
     @Transactional
     public Long join(CreateMemberDto createMemberDto) {
@@ -28,21 +34,20 @@ public class MemberService {
             throw new DuplicateMemberException("이미 가입되어 있는 유저입니다.");
         }
 
-        // TODO -> Department 부서명 연결 필요
-        Department department = Department.builder()
-                .name("부서명")
-                .build();
-
-        departmentRepository.save(department);
-
         Member member = Member.builder()
                 .username(createMemberDto.getUsername())
                 .password(bCryptPasswordEncoder.encode(createMemberDto.getPassword()))
-                .department(department)
+                .department(departmentRepository.findByName(createMemberDto.getDepartmentName())
+                        .orElseThrow(() -> new RuntimeException("Could not find Department by "+ createMemberDto.getDepartmentName())))
                 .role(Role.ROLE_USER)
                 .build();
 
         return memberRepository.save(member).getId();
     }
 
+    public MemberDto findByUsername(String username) {
+        return memberRepository.findByUsername(username)
+                .map(converter::convertMemberDto)
+                .orElseThrow(()->new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
 }
